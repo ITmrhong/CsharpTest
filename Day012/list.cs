f895f8c8498ec88d0f0335593f7cc4822776b8ce
ITmrhong <137870172+ITmrhong@users.noreply.github.com>
2023-07-12 14:25:30 +0900

    리스트 insert
    리스트 insert

---
+++ Day012/list.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+     class Dog
+    {
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list= new List<int>();
+            for (int i = 0; i < 5; i++)
+                list.Add(i);
+            foreach (int element in list)
+                Console.Write(element + " ");
+            Console.WriteLine();
+            list.Insert(2, 2);
+            foreach (int element in list)
+                Console.Write(element + " ");
+        }
+    }
+}
